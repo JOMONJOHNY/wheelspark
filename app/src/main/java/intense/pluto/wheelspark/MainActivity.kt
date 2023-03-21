ea0271973ade2ee72e681080ad3f89c6f3a03f81
Abhiram <abhiramns345@gmail.com>
2023-03-21 01:13:17 -0700

    commit first c

--- app/src/main/java/intense/pluto/wheelspark/MainActivity.kt
+++ app/src/main/java/intense/pluto/wheelspark/MainActivity.kt
@@ -1,43 +1,54 @@
 package intense.pluto.wheelspark
 
 import android.os.Bundle
+import androidx.compose.ui.res.painterResource
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import intense.pluto.wheelspark.ui.theme.WheelsParkTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WheelsParkTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Intro()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun Intro() {
+        val bgCar =painterResource(R.drawable.bgcar)
+        val logo = painterResource(R.drawable.logo_large)
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WheelsParkTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = Color.White // Use a solid color instead of MaterialTheme for performance
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Image(
+                    painter = bgCar,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+                Image(
+                    painter = logo,
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+            }
+        }
     }
 }
